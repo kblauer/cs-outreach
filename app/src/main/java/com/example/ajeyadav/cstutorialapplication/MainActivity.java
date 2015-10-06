4e426b57cbe8f7ab4a4e73094d8e171bf26b558f
Ajeet Yadav <Achzeet@gmail.com>
2015-10-06 11:41:36 -0500

    Initial UI

---
+++ app/src/main/java/com/example/ajeyadav/cstutorialapplication/MainActivity.java
@@ -0,0 +1,83 @@
+package com.example.ajeyadav.cstutorialapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
+import android.view.View.OnClickListener;
+
+
+public class MainActivity extends AppCompatActivity implements OnClickListener {
+
+    Button button1, button2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        button1 = (Button)findViewById(R.id.buttonlogin);
+        button1.setOnClickListener(this);
+
+        button2 = (Button)findViewById(R.id.buttonsignup);
+        button2.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
+        if(v.getId() == R.id.buttonlogin) {
+
+            Intent i = new Intent(this, login.class);
+            startActivity(i);
+        }
+
+        else if (v.getId() == R.id.buttonsignup) {
+
+            Intent i = new Intent(this, signup.class);
+            startActivity(i);
+        }
+    }
+}
